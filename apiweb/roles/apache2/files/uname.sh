e6ee48170a51cf4a078dc9e6dd7d37cfb000e301
JavierLario77 <kelpiefloyd2@gmail.com>
2021-03-22 13:32:06 +0100

    Proyecto ansible acabado

---
+++ apiweb/roles/apache2/files/uname.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+config_apache(){
+    #sed -i "s/\${APACHE_RUN_USER}/\www-data/g" /etc/apache2/apache2.conf
+    #sed -i "s/${APACHE_RUN_DIR}/$/g" /etc/apache2/apache2.conf
+    #asocia las variables del apache2.conf con los valores definidos en /etc/apache2/envars
+    source /etc/apache2/envvars
+    # con apache2 -S veremos los valores del las variables asociadas
+
+    # Apache gets grumpy about PID files pre-existing
+    #rm -f /var/run/apache2/apache2.pid
+}
+
+config_apache
